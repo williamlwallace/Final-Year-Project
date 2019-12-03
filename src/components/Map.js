63ce5a9148e6123374069b378a48c1290e2dd70e
Ben Adams <darwinzero@gmail.com>
2019-12-03 20:16:27 +0000

    pulsing dot

--- src/components/Map.js
+++ src/components/Map.js
@@ -38,6 +38,52 @@ class Map extends Component {
             style: 'mapbox://styles/mapbox/dark-v10'
         });
         var theMap = this.map;
+        const dotSize = 48;
+        const pulsingDot = {
+            width: dotSize,
+            height: dotSize,
+            data: new Uint8Array(dotSize * dotSize * 4),
+
+            onAdd: function() {
+                var canvas = document.createElement('canvas');
+                canvas.width = this.width;
+                canvas.height = this.height;
+                this.context = canvas.getContext('2d');
+            },
+
+            render: function() {
+                const duration = 3000;
+                const t = (performance.now() % duration) / duration;
+
+                const radius = dotSize / 2 * 0.3;
+                const outerRadius = dotSize / 2 * 0.5 * t + radius;
+                const context = this.context;
+
+                // draw outer circle
+                context.clearRect(0, 0, this.width, this.height);
+                context.beginPath();
+                context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
+                context.fillStyle = 'rgba(200, 255, 200,' + (1 - t) + ')';
+                context.fill();
+
+                // draw inner circle
+                context.beginPath();
+                context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
+                context.fillStyle = 'rgba(50, 128, 50, 1)';
+                context.strokeStyle = 'white';
+                context.lineWidth = 1 + 2 * (1 - t);
+                context.fill();
+                context.stroke();
+
+                // update this image's data with data from the canvas
+                this.data = context.getImageData(0, 0, this.width, this.height).data;
+
+                // keep the map repainting
+                theMap.triggerRepaint();
+                // return `true` to let the map know that the image was updated
+                return true;
+            }
+        };
 
         this.map.on('load', () => {
             // initialize the institutionPoints data source
